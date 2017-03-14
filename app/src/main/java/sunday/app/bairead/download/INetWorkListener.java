31534ab321c08d4492d027a1b7f042fde56f9ed9
sunday <szflone521@hotmail.com>
2017-03-14 11:16:33 +0800

    delete capitals folder

--- app/src/main/java/sunday/app/bairead/download/INetWorkListener.java
+++
@@ -1,9 +0,0 @@
-package sunday.app.bairead.download;
-
-/**
- * Created by sunday on 2017/3/13.
- */
-
-public interface INetWorkListener {
-    void unConnect();
-}
--- app/src/main/java/sunday/app/bairead/tool/AlarmTickManager.java
+++
@@ -1,8 +0,0 @@
-package sunday.app.bairead.tool;
-
-/**
- * Created by sunday on 2016/12/6.
- */
-
-public class AlarmTickManager {
-}
